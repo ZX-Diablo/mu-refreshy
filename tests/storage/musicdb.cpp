b9dfb4159a846eb5c48fc311ab159f206e6c9c61
ZX.Diablo <senseyfollower@gmail.com>
2016-11-09 00:27:54 +0500

    musicdb: empty test

--- tests/storage/musicdb.cpp
+++ tests/storage/musicdb.cpp
@@ -3,6 +3,10 @@
 
 #include <storage/musicdb.h>
 
-BOOST_AUTO_TEST_CASE(empty)
-{
-}
+BOOST_AUTO_TEST_SUITE(fill);
+	BOOST_AUTO_TEST_CASE(empty)
+	{
+		musicdb db((std::string()));
+		BOOST_REQUIRE_NO_THROW(db.fill(nullptr));
+	}
+BOOST_AUTO_TEST_SUITE_END();
