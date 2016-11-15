0813339e1f60b6f3ee9eb5ce7b143112f64d2b9c
ZX.Diablo <senseyfollower@gmail.com>
2016-11-16 01:08:41 +0500

    musicbrainz: code fixes, test finished

--- src/storage/remote/musicbrainz.cpp
+++ src/storage/remote/musicbrainz.cpp
@@ -18,40 +18,52 @@ musicbrainz::musicbrainz (const std::string& user_agent)
 artist_list_t musicbrainz::search_artists (const std::string& artist, const std::string& release) const
 {
 	artist_list_t result;
-	MusicBrainz5::CMetadata data = this->query.Query("release-group", "", "",
-		{ {"query", "artist:\"" + artist + "\" AND releasegroup:\"" + release + "\""} }); // No way to search for exact name match?
-	MusicBrainz5::CReleaseGroupList* releases = data.ReleaseGroupList();
 
-	if (releases == nullptr || releases->NumItems() == 0)
+	if (artist.empty() || release.empty())
 	{
 		return result;
 	}
 
-	for (unsigned int i = 0; i < static_cast<unsigned int>(releases->NumItems()); i++)
+	try
 	{
-		MusicBrainz5::CReleaseGroup* rg = releases->Item(i);
-		MusicBrainz5::CArtistCredit* ac = rg->ArtistCredit();
+		MusicBrainz5::CMetadata data = this->query.Query("release-group", "", "",
+			{ {"query", "artist:\"" + artist + "\" AND releasegroup:\"" + release + "\""} }); // No way to search for exact name match?
+		MusicBrainz5::CReleaseGroupList* releases = data.ReleaseGroupList();
 
-		if (ac == nullptr || ac->NameCreditList() == nullptr || ac->NameCreditList()->NumItems() == 0)
+		if (releases == nullptr || releases->NumItems() == 0)
 		{
-			continue;
+			return result;
 		}
 
-		MusicBrainz5::CNameCredit* nc = ac->NameCreditList()->Item(0); // Hopefully it's ok to take the first artist
-
-		if (nc == nullptr)
+		for (unsigned int i = 0; i < static_cast<unsigned int>(releases->NumItems()); i++)
 		{
-			continue;
+			MusicBrainz5::CReleaseGroup* rg = releases->Item(i);
+			MusicBrainz5::CArtistCredit* ac = rg->ArtistCredit();
+
+			if (ac == nullptr || ac->NameCreditList() == nullptr || ac->NameCreditList()->NumItems() == 0)
+			{
+				continue;
+			}
+
+			MusicBrainz5::CNameCredit* nc = ac->NameCreditList()->Item(0); // Hopefully it's ok to take the first artist
+
+			if (nc == nullptr)
+			{
+				continue;
+			}
+
+			MusicBrainz5::CArtist* a = nc->Artist();
+
+			if (a == nullptr)
+			{
+				continue;
+			}
+
+			result.push_back(artistfactory::get(a));
 		}
-
-		MusicBrainz5::CArtist* a = nc->Artist();
-
-		if (a == nullptr)
-		{
-			continue;
-		}
-
-		result.push_back(artistfactory::get(a));
+	}
+	catch (...)
+	{
 	}
 
 	return result;
@@ -61,7 +73,12 @@ release_set_t musicbrainz::search_releases (const std::string& artist_id) const
 {
 	release_set_t result;
 
-	if (!artist_id.empty())
+	if (artist_id.empty())
+	{
+		return result;
+	}
+
+	try
 	{
 		MusicBrainz5::CMetadata data = this->query.Query("release-group", "", "", { {"artist", artist_id} });
 		MusicBrainz5::CReleaseGroupList* releases = data.ReleaseGroupList();
@@ -77,6 +94,9 @@ release_set_t musicbrainz::search_releases (const std::string& artist_id) const
 
 			result.insert(releasefactory::get(rg));
 		}
+	}
+	catch (...)
+	{
 	}
 
 	return result;
--- tests/storage/remote/musicbrainz.cpp
+++ tests/storage/remote/musicbrainz.cpp
@@ -5,6 +5,12 @@
 #include <test/defines.h>
 
 const std::string USER_AGENT = "mu-refreshy/test (https://github.com/ZX-Diablo/mu-refreshy)";
+const std::string ARTIST = "Death";
+const std::string RELEASE = "Human";
+const std::string RELEASE_NOT_EXISTING = "qwerty";
+
+const std::string ARTIST_ID = "dbb3b771-ae77-4381-b61c-758b5b7898ec";
+const std::string ARTIST_INVALID_ID = "123";
 
 BOOST_AUTO_TEST_SUITE(search,
 	* boost::unit_test::enable_if<BOOST_TEST_INTEGRATION_ENABLED>()
@@ -17,6 +23,20 @@ BOOST_AUTO_TEST_SUITE(search,
 			artist_list_t a = m.search_artists(std::string(), std::string());
 			BOOST_TEST(a.empty());
 		}
+
+		BOOST_AUTO_TEST_CASE(not_existing)
+		{
+			musicbrainz m(USER_AGENT);
+			artist_list_t a = m.search_artists(ARTIST, RELEASE_NOT_EXISTING);
+			BOOST_TEST(a.empty());
+		}
+
+		BOOST_AUTO_TEST_CASE(search_success)
+		{
+			musicbrainz m(USER_AGENT);
+			artist_list_t a = m.search_artists(ARTIST, RELEASE);
+			BOOST_TEST(!a.empty());
+		}
 	BOOST_AUTO_TEST_SUITE_END();
 
 	BOOST_AUTO_TEST_SUITE(search_releases);
@@ -25,6 +45,20 @@ BOOST_AUTO_TEST_SUITE(search,
 			musicbrainz m(USER_AGENT);
 			release_set_t r = m.search_releases(std::string());
 			BOOST_TEST(r.empty());
+		}
+
+		BOOST_AUTO_TEST_CASE(invalid_id)
+		{
+			musicbrainz m(USER_AGENT);
+			release_set_t r = m.search_releases(ARTIST_INVALID_ID);
+			BOOST_TEST(r.empty());
+		}
+
+		BOOST_AUTO_TEST_CASE(search_success)
+		{
+			musicbrainz m(USER_AGENT);
+			release_set_t r = m.search_releases(ARTIST_ID);
+			BOOST_TEST(!r.empty());
 		}
 	BOOST_AUTO_TEST_SUITE_END();
 BOOST_AUTO_TEST_SUITE_END();
