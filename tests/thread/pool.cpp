ba411ec21b49371ddc8263f39a5bf9a175b8d345
ZX.Diablo <senseyfollower@gmail.com>
2016-11-18 00:29:49 +0500

    thread pool test (kind of)

--- tests/thread/pool.cpp
+++ tests/thread/pool.cpp
@@ -3,9 +3,36 @@
 
 #include <thread/pool.h>
 
+const unsigned int POOL_SIZE = 2;
+
 BOOST_AUTO_TEST_SUITE(constructor);
 	BOOST_AUTO_TEST_CASE(empty)
 	{
-		pool p(0);
+		BOOST_REQUIRE_NO_THROW(pool(0));
+	}
+
+	BOOST_AUTO_TEST_CASE(nonempty)
+	{
+		BOOST_REQUIRE_NO_THROW(pool(POOL_SIZE));
+	}
+BOOST_AUTO_TEST_SUITE_END();
+
+BOOST_AUTO_TEST_SUITE(add_task);
+	BOOST_AUTO_TEST_CASE(add)
+	{
+		pool p;
+		std::thread::id thread_id = std::this_thread::get_id();
+		p.add_task([thread_id]()
+		{
+			BOOST_TEST(std::this_thread::get_id() != thread_id);
+		});
+	}
+BOOST_AUTO_TEST_SUITE_END();
+
+BOOST_AUTO_TEST_SUITE(wait_tasks);
+	BOOST_AUTO_TEST_CASE(wait)
+	{
+		pool p;
+		BOOST_REQUIRE_NO_THROW(p.wait());
 	}
 BOOST_AUTO_TEST_SUITE_END();
