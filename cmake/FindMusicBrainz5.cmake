7faa7e57572afabb18da2d76be8b49d3584ecc2f
ZX.Diablo <senseyfollower@gmail.com>
2022-09-12 22:16:18 +0200

    Proper musicbrainz5 library search and linkage to fix build on Ubuntu

--- cmake/FindMusicBrainz5.cmake
+++ cmake/FindMusicBrainz5.cmake
@@ -1,7 +1,11 @@
 # - Try to find libmusicbrainz5
 
 find_path(LIBMUSICBRAINZ5_INCLUDE_DIR Query.h PATH_SUFFIXES musicbrainz5)
-find_library(LIBMUSICBRAINZ5_LIBRARY NAMES musicbrainz5 libmusicbrainz5)
+
+find_library(LIBMUSICBRAINZ5_LIBRARY NAMES musicbrainz5cc libmusicbrainz5cc)
+if(NOT LIBMUSICBRAINZ5_LIBRARY)
+	find_library(LIBMUSICBRAINZ5_LIBRARY NAMES musicbrainz5 libmusicbrainz5)
+endif()
 
 set(LIBMUSICBRAINZ5_INCLUDE_DIRS ${LIBMUSICBRAINZ5_INCLUDE_DIR})
 set(LIBMUSICBRAINZ5_LIBRARIES ${LIBMUSICBRAINZ5_LIBRARY})
